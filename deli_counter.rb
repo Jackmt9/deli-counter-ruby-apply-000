d99b5ad6971dd4789ffb7c9f1c432ecd391d3d49
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-23 23:18:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,13 +2,13 @@ require "pry"
 # Write your code here.
 $katz_deli = []
 def line
+  binding.pry
   if katz_deli.length == 0
     puts "The line is currently empty"
   else katz_deli.each do |customer|
       puts katz_deli[customer].index
       index = count + 1
       puts "#{index}. #{katz_deli}"
-      binding.pry
     end 
   end 
 end 

84d179b529f2c8753b8fcb4a70d7e758bdc66ee0
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-23 23:49:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,13 @@
 $katz_deli = ["Jack", "Sean"]
 
 def line(katz_deli)
+  phrase = "The line is currently:"
   if katz_deli.length > 0
-    katz_deli
+    katz_deli.each_with_index do |name, index|
+      phrase += " #{index + 1}. #{name}"
+    end
+    puts phrase
+  else
+    puts "The line is currently empty."
+  end 
+end

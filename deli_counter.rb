1ef5509a02bc0ae1f7f1478a849d773ac6ce3c73
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-23 23:14:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,9 @@
 # Write your code here.
 $katz_deli = []
 def line
-  if (katz_deli.length>0){
+  if katz_deli.length > 0
     puts "The line is currently:"
-    count = 0
-    if(count<katz_deli.length){
+    if (count < katz_deli.length){
       index = count + 1
-      puts " ${index}. ${katz_deli[count]"
-      count++
-    }
+      puts " #{index}. #{katz_deli"
+      

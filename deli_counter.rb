4ebcdb38ffb628b01cd5fcc59a8d66d56aed09e1
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-23 23:23:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,11 @@ require "pry"
 # Write your code here.
 $katz_deli = []
 def line
-  binding.pry
   if katz_deli.length == 0
     puts "The line is currently empty"
   else katz_deli.each do |customer|
-      puts katz_deli[customer].index
+      puts "The line is currently: 
+      #{katz_deli.find_index(customer) + 1)} + "." + #{customer}"
       index = count + 1
       puts "#{index}. #{katz_deli}"
     end 

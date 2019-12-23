0029559bbbb6f1bbf4029b665f45868cc615ef99
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-23 23:11:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,3 +3,9 @@ $katz_deli = []
 def line
   if (katz_deli.length>0){
     puts "The line is currently:"
+    count = 0
+    if(count<katz_deli.length){
+      index = count + 1
+      puts " ${index}. ${katz_deli[count]"
+      count++
+    }

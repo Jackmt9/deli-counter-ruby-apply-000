8a2d590a67019037c76e9f7bea3b27ac5034965c
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-23 23:39:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     if katz_deli.length == 0
     puts "The line is currently empty."
     else
-      puts "The line is currently: #{line_order(katz_deli) "
+      puts "The line is currently: #{line_order(katz_deli) 
     end 
 end 
 

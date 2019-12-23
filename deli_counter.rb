fc973d614e4c7c7c9e56159c50733a2b5c187b54
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-23 23:16:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 # Write your code here.
 $katz_deli = []
 def line
-  if katz_deli.length > 0
-    puts "The line is currently:"
-    if (count < katz_deli.length){
+  if katz_deli.length == 0
+    puts "The line is currently empty"
+  else katz_deli.each do |customer|
+      puts customer.index
       index = count + 1
       puts " #{index}. #{katz_deli"
       
